ed98c0f1abbd1e464054802c31e5139990b09a49
yatsenko.v <FDT3sRtwD>
2023-01-09 14:41:42 +0300

    Initial commit

---
+++ src/main/java/ru/maxima/springmvc/HelloController.java
@@ -0,0 +1,13 @@
+package ru.maxima.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello_world";
+    }
+}
